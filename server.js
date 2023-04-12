35f127a47b6e7f02e697429da1f4af8b5f1680f2
josh-benton <120067603+josh-benton@users.noreply.github.com>
2023-04-12 11:45:25 -0400

    updated server.js to handle post request

--- server.js
+++ server.js
@@ -8,10 +8,14 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
+app.use(express.json());
+
+//test get handler
 app.get("/test", (req, res) => {
   res.send("Hello World!");
 });
 
+//get handler for api
 app.get("/api/tasks", (req, res) => {
     pool.query("SELECT * FROM todo_list", (err, result) => {
         if (err) {
@@ -22,6 +26,28 @@ app.get("/api/tasks", (req, res) => {
         }
     })
 })
+
+//post handler
+app.post("/api/tasks", (req, res) => {
+    console.log(req.body);
+    const { task } = req.body;
+    pool.query(
+      "INSERT INTO todo_list (task) VALUES ($1) RETURNING *",
+      [task],
+      (err, result) => {
+        if (err) {
+          console.error(err);
+          res.status(500).json({ error: "Internal server error" });
+        } else {
+          res.status(201).json(result.rows[0]);
+        }
+      }
+    );
+  });
+
+//put handler
+
+//delete handler
 
 app.listen(PORT, () => {
   console.log("Server is listening on port 3000...");
