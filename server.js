9f06f1032f1b72ff8d4c19aa592b5d94830692ca
josh-benton <120067603+josh-benton@users.noreply.github.com>
2023-04-12 11:34:52 -0400

    updated get request for api route

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const PORT = process.env.PORT || 3000;
+const dotenv = require("dotenv").config();
+const { Pool } = require("pg");
 
-app.get('/', (req, res) => {
-res.send('Hello World!')
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+app.get("/test", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/api/tasks", (req, res) => {
+    pool.query("SELECT * FROM todo_list", (err, result) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send("Error querying the database");
+        } else {
+            res.send(result.rows);
+        }
+    })
 })
 
 app.listen(PORT, () => {
-console.log('Server is listening on port 3000...')
-})
+  console.log("Server is listening on port 3000...");
+});
